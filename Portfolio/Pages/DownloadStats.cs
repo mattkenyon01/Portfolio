2d31815cb8d084205a0f06c9250f62d9fe87ec92
MattKenyon03 <165806104+mattkenyon01@users.noreply.github.com>
2025-07-29 17:11:43 +0100

    Update

---
+++ Portfolio/Pages/DownloadStats.cs
@@ -0,0 +1,14 @@
+﻿namespace Portfolio.Pages
+{
+    public static class DownloadStats
+    {
+        public static int CastleClimbersDownloads => 187;
+        public static int BucketConnectDownloads => 255;
+        public static int GirlsWhoWalkDownloads => 82;
+
+        public static int InstagramFollowers = 114;
+
+        public static int TotalDownloads => CastleClimbersDownloads + BucketConnectDownloads + GirlsWhoWalkDownloads;
+
+    }
+}
---
+++ Portfolio/wwwroot/js/chart.js
@@ -0,0 +1,25 @@
+﻿window.renderChart = function (climbers, bucket, girls) {
+    const ctx = document.getElementById('firebaseChart')?.getContext('2d');
+    if (!ctx) {
+        console.error("Canvas not found");
+        return;
+    }
+
+    new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['Castle Climbers', 'Bucket Connect', 'Girls Who Walk'],
+            datasets: [{
+                data: [climbers, bucket, girls],
+                backgroundColor: ['#911f1f', '#41d5c3', '#eda193'],
+                borderWidth: 0
+            }]
+        },
+        options: {
+            cutout: '65%',
+            plugins: {
+                legend: { display: false }
+            }
+        }
+    });
+};
