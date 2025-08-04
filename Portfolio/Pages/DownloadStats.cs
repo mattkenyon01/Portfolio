d528853488dd7fc13edd2295fca1076f25c8c216
MattKenyon03 <165806104+mattkenyon01@users.noreply.github.com>
2025-08-04 22:24:52 +0100

    Pre holiday

--- Portfolio/Pages/DownloadStats.cs
+++ Portfolio/Pages/DownloadStats.cs
@@ -3,10 +3,10 @@
     public static class DownloadStats
     {
         public static int CastleClimbersDownloads => 187;
-        public static int BucketConnectDownloads => 255;
-        public static int GirlsWhoWalkDownloads => 82;
+        public static int BucketConnectDownloads => 260;
+        public static int GirlsWhoWalkDownloads => 88;
 
-        public static int InstagramFollowers = 114;
+        public static int InstagramFollowers = 115 + 8;
 
         public static int TotalDownloads => CastleClimbersDownloads + BucketConnectDownloads + GirlsWhoWalkDownloads;
 
